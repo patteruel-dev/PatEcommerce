349fde8a010d7063829e79e4f7762240b2c98c5a
John Patrick Teruel <jpteruel95@gmail.com>
2023-03-13 13:51:04 +0800

    Initialized SwiftUI Project

---
+++ PatEcommerce/PatEcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatEcommerceApp.swift
+//  PatEcommerce
+//
+//  Created by John Patrick Teruel on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct PatEcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-PatEcommerceTests/Pods-PatEcommerceTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PatEcommerceTests : NSObject
+@end
+@implementation PodsDummy_Pods_PatEcommerceTests
+@end
